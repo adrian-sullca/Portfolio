cc4c2d0fde560fc701359afe6e75a54e0cd47363
PABLO ROS PROFE <pros7@xtec.cat>
2024-10-31 18:18:07 +0100

    base afegit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./public", // Root directory for Vite
+  base: "./", // Use relative paths for assets
   build: {
     outDir: "../dist", // Output directory for the build
     rollupOptions: {
