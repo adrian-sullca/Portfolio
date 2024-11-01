54566e50b0821f5f9852cca7fcf2518510041c79
PABLO ROS PROFE <pros7@xtec.cat>
2024-11-01 20:16:46 +0100

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   root: "./", // Root directory for Vite
   base: "./", // Use relative paths for assets
   build: {
-    outDir: "./dist", // Output directory for the build
+    outDir: "dist", // Output directory for the build
     rollupOptions: {
       input: "./index.html", // Entry point for the build
     },
